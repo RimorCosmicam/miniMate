f38963bf1f75b199b8faa72a4b8ae46baec9f7c4
Rimor <114653635+RimorCosmicam@users.noreply.github.com>
2026-08-30 23:15:02 -0300

    fix: missing sp import

--- app/src/main/java/com/minimate/ui/TouchpadScreen.kt
+++ app/src/main/java/com/minimate/ui/TouchpadScreen.kt
@@ -37,6 +37,7 @@ import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.minimate.bluetooth.BluetoothAudioBridge
 import com.minimate.bluetooth.BluetoothHidManager
 import com.minimate.bluetooth.WebcamCapture
